27771b811fff69964a06afa7350a844e8eadd4cd
KondiPrint <Marvin.kn@protonmail.com>
2024-01-29 13:58:43 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,11 @@
 'use client';
 import { useState, useEffect } from 'react';
 import useRequestData from '@/components/hooks/useRequestData';
-import Header from '../layout/Header';
+import Header from '@/layout/Header';
 import { TbArrowRightCircle } from 'react-icons/tb';
 import { FaWifi } from 'react-icons/fa';
 import { FaShower } from 'react-icons/fa6';
-import Slider from '../app/components/slider';
+import Slider from '@/components/slider';
 
 export default function Home() {
   const { data, isLoading, error, makeRequest } = useRequestData();
