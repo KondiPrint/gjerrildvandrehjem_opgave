f5fcd518905100763d73c8e0229068dd010c3a35
KondiPrint <Marvin.kn@protonmail.com>
2024-02-07 09:50:21 +0100

    forsøg

---
+++ src/components/Galleri/GalleriContent.jsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { useState } from 'react';
+
+export function GalleriSlider({ data }) {
+  const [open, setOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleOpen = (image) => {
+    setSelectedImage(image);
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setSelectedImage(null);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <div className='grid grid-cols-2 gap-5 md:grid-cols-4'>
+        {data &&
+          data.content.map((e, index) => (
+            <Card key={index} className='h-64 cursor-pointer overflow-hidden transition-opacity hover:opacity-90' onClick={() => handleOpen(e.image)}>
+              <img alt='nature' className='h-full w-full object-cover object-center' src={`../../assets/images/Galleri/${e.image}`} onClick={() => handleOpen(e.image)} />
+            </Card>
+          ))}
+        {selectedImage && (
+          <Dialog size='xl' open={open} handler={handleClose}>
+            <DialogBody>
+              <img alt='nature' className='h-[48rem] w-full rounded-lg object-cover object-center' src={`../../assets/images/Galleri/${selectedImage}`} />
+            </DialogBody>
+          </Dialog>
+        )}
+      </div>
+    </>
+  );
+}
--- src/components/Galleri/GalleriSlider.jsx
+++ src/components/Galleri/GalleriSlider.jsx
@@ -1,45 +1,29 @@
-'use client'
+import { Carousel } from '@material-tailwind/react';
 
-import { Carousel } from "@material-tailwind/react";
-import data from "../../../public/assets/json_files/galleri.json"
-
-
-
-export function GalleriSlider() {
-
-  console.log(data.content);
-
+export function GalleriSlider({ data }) {
   return (
-
     <>
+      <h1 className='font-main_font text-center mt-5 uppercase font-medium text-3xl tracking-widest bg-reseda py-6 text-white'>Galleri</h1>
 
-    <h1 className="font-main_font text-center mt-5 uppercase font-medium text-3xl tracking-widest bg-reseda py-6 text-white">Galleri</h1>
-
-    <Carousel
-      className="rounded-xl h-96 w-fit mx-3 my-5 lg:h-[600px]"
-      autoplay={true}
-      navigation={({ setActiveIndex, activeIndex, length }) => (
-        <div className="absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2">
-          {new Array(length).fill("").map((_, i) => (
-            <span
-              key={i}
-              className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? "w-8 bg-white" : "w-4 bg-white/50"
-                }`}
-              onClick={() => setActiveIndex(i)}
-            />
-          ))}
-        </div>
-      )}
-    >
-      {data &&
-        data.content.map((e, i) => {
-          return (
-            <img key={i} src={`../../assets/images/Galleri/${e.image}`} alt="" className="h-full w-full object-cover" />
-          )
-        })
-      }
-    </Carousel>
+      <Carousel
+        className='rounded-xl h-96 w-fit mx-3 my-5 lg:h-[600px]'
+        autoplay={true}
+        navigation={({ setActiveIndex, activeIndex, length }) => (
+          <div className='absolute bottom-4 left-2/4 z-50 flex -translate-x-2/4 gap-2'>
+            {new Array(length).fill('').map((_, i) => (
+              <span
+                key={i}
+                className={`block h-1 cursor-pointer rounded-2xl transition-all content-[''] ${activeIndex === i ? 'w-8 bg-white' : 'w-4 bg-white/50'}`}
+                onClick={() => setActiveIndex(i)}
+              />
+            ))}
+          </div>
+        )}>
+        {data &&
+          data.content.map((e, i) => {
+            return <img key={i} src={`../../assets/images/Galleri/${e.image}`} alt='' className='h-full w-full object-cover' />;
+          })}
+      </Carousel>
     </>
-
   );
 }
