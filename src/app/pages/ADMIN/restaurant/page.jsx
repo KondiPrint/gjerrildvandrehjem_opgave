eb99d49e2e62ad34e8d8fcf9d4d67a7f63a9b1ab
Andreas G. Christensen <48524202+Andreas-DEV@users.noreply.github.com>
2024-02-07 13:35:15 +0100

    Added restaurant admin

--- src/app/pages/ADMIN/restaurant/page.jsx
+++ src/app/pages/ADMIN/restaurant/page.jsx
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { Textarea, Input, Alert } from '@material-tailwind/react';
 import Loader from '@/components/Loader';
 
-export default function kattegatADMIN() {
+export default function restaurantADMIN() {
   const { data, isLoading, error, makeRequest } = useRequestData();
   const { data: dataPUT, isLoading: isLoadingPUT, error: errorPUT, makeRequest: makeRequestPUT } = useRequestData();
 
@@ -15,32 +15,35 @@ export default function kattegatADMIN() {
   const [subTitle, setSubTitle] = useState('');
   const [content, setContent] = useState('');
   const [content2, setContent2] = useState('');
+  const [content3, setContent3] = useState('');
+  const [content4, setContent4] = useState('');
 
   useEffect(() => {
-    makeRequest(`https://gjerrildapi.onrender.com/home`, 'GET', null);
+    makeRequest(`https://gjerrildapi.onrender.com/restaurant`, 'GET', null);
   }, []);
 
   useEffect(() => {
     if (data) {
       setTitle(data.content.title);
-      setSubTitle(data.content.subtitle);
       setContent(data.content.content);
       setContent2(data.content.content2);
+      setContent3(data.content.content2);
+      setContent4(data.content.content2);
     }
   }, [data]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    const redigeretText = { content: content, content2: content2, title: title, subTitle: subTitle };
+    const redigeretText = { content: content, content2: content2, content3: content3, content4: content4, title: title, subTitle: subTitle };
 
-    makeRequestPUT(`https://gjerrildapi.onrender.com/home/${e.target.inpID.value}`, 'PUT', redigeretText);
+    makeRequestPUT(`https://gjerrildapi.onrender.com/restaurant/${e.target.inpID.value}`, 'PUT', redigeretText);
   };
   return (
     <>
       <Header />
 
-      <h1 className='text-center text-4xl font-semibold my-5'>Rediger siden HOME</h1>
+      <h1 className='text-center text-4xl font-semibold my-5'>Rediger siden RESTAURANT</h1>
 
       {(error || errorPUT) && <h2>Error ...</h2>}
       {(isLoading || isLoadingPUT) && <Loader />}
@@ -64,12 +67,11 @@ export default function kattegatADMIN() {
               <div className='max-w-xl my-5'>
                 <Input label='Titel' defaultValue={e.title} className='bg-white' onInput={(e) => setTitle(e.target.value)} />
               </div>
-              <div className='max-w-xl'>
-                <Input label='Undertitel' defaultValue={e.subtitle} className='bg-white' onInput={(e) => setSubTitle(e.target.value)} />
-              </div>
               <div className='max-w-4xl my-5 flex gap-5'>
                 <Textarea label='Body1' defaultValue={e.content} name='txtContent' id='txtContent' onInput={(e) => setContent(e.target.value)} className='h-48 bg-white' />
                 <Textarea label='Body2' defaultValue={e.content2} name='txtContent' id='txtContent' onInput={(e) => setContent2(e.target.value)} className='h-48 bg-white' />
+                <Textarea label='Body3' defaultValue={e.content3} name='txtContent' id='txtContent' onInput={(e) => setContent3(e.target.value)} className='h-48 bg-white' />
+                <Textarea label='Body4' defaultValue={e.content4} name='txtContent' id='txtContent' onInput={(e) => setContent4(e.target.value)} className='h-48 bg-white' />
               </div>
               <button type='submit' className='btn btn-primary h-fit w-fit'>
                 Færdiggør
--- src/app/pages/restaurant/page.jsx
+++ src/app/pages/restaurant/page.jsx
@@ -1,26 +1,30 @@
+'use client'
+
 import RestaurantHero from '@/components/Restaurant/RestaurantHero';
+import React, { useState, useEffect } from 'react';
 import Header from '@/layout/Header';
+import useRequestData from '@/components/hooks/useRequestData';
 
 export default function restaurant() {
+  const { data, isLoading, error, makeRequest } = useRequestData();
+
+  useEffect(() => {
+    makeRequest(`https://gjerrildapi.onrender.com/restaurant`, 'GET', null);
+  }, []);
+
   return (
     <>
       <RestaurantHero />
 
-      <article className='container mx-auto px-4 font-main_font'>
-        <p>
-          På Gjerrild Vandrerhjem er vi utroligt glade for mad! Hvad end du har af ønsker, så kan vi kreere det. Vi går op i den gode smag og de gode råvarer og tager ofte verden rundt på vores
-          buffeter. Vi går op i at så meget som muligt er hjemmelavet og er glade for hjemmebag og at sylte.
-        </p>
-        <p>
-          Vi bruger mange grøntsager og krydderier og du vil derfor opleve en buffet med rigtig mange forskellige og velsmagende retter. Der er altid gode salater på buffeten og du vil helt sikkert
-          blive præsenteret for noget, du ikke har prøvet før.
-        </p>
-        <p>
-          Vi kører i højsæsonen buffetkoncept, hvor buffetbordet bugner af kolde og varme retter. Det er dog altid muligt at bestille bord til 6+ personer, også til en enkelt ret eller kaffe/kage.
-          Ring til os på 40224199, hvis du vil høre mere. Vi laver morgenmad, brunch, frokost, aftensmad, kage/konfekt og snacks til alle lejligheder.
-        </p>
-        <p>Vi tager altid gerne hensyn til allergier.</p>
-      </article>
+      {data &&
+        <article className='container mx-auto px-4 font-main_font'>
+
+          <p>{data.content[0].content}</p>
+          <p>{data.content[0].content2}</p>
+          <p>{data.content[0].content3}</p>
+          <p>{data.content[0].content4}</p>
+        </article>
+      }
     </>
   );
 }
